a47d108a11c9312ccb93fccf57e7541edc11a8da
Mamoun Gharbi <magharbi@chindi.laas.fr>
2015-10-29 16:09:30 +0100

    init git node

---
+++ src/gtp_ros_bridge_node.cpp
@@ -0,0 +1,375 @@
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <gtp_ros_msg/requestAction.h>
+#include <msgClient.hh>
+#include <jsoncpp/json/json.h>
+
+#include <string>
+#include <iostream>
+
+#include "std_msgs/String.h"
+#include <gtp_ros_msg/GTPTraj.h>
+#include <trajectory_msgs/JointTrajectoryPoint.h>
+
+msgClient client;
+ros::Publisher trajectory;
+
+class GtpRosBridge
+{
+protected:
+
+  ros::NodeHandle nh_;
+  // NodeHandle instance must be created before this line. Otherwise strange error may occur.
+  actionlib::SimpleActionServer<gtp_ros_msg::requestAction> as_; 
+  std::string action_name_;
+  // create messages that are used to published feedback/result
+  gtp_ros_msg::requestFeedback feedback_;
+  gtp_ros_msg::requestResult result_;
+  
+
+  
+
+
+public:
+
+  GtpRosBridge(std::string name) :
+    as_(nh_, "gtp_ros_server", boost::bind(&GtpRosBridge::executeCB, this, _1), false),
+    action_name_(name)
+  {
+    as_.start();
+  }
+
+  ~GtpRosBridge(void)
+  {
+  }
+
+  void executeCB(const gtp_ros_msg::requestGoalConstPtr &goal)
+  {
+    // helper variables
+    ros::Rate r(1);
+    bool success = true;
+    ROS_INFO("%s: Computing the request", action_name_.c_str());
+    
+
+    //int f = str.find("taskAlternativeId");
+    //std::cout << str.c_str()[f] << std::endl; //163
+    //int vir = str.rfind(",");
+    //std::cout << vir << std::endl; //164
+    
+
+
+        
+        
+    //std::string altIdStr = str.substr(str.find("taskAlternativeId") + 19, str.rfind(",") - str.find("taskAlternativeId") - 19);
+    //std::string IdStr = str.substr(str.find("taskId") + 8, str.rfind("}") - 1 - str.find("taskId") - 8);
+    
+    Json::FastWriter wrt;
+    if (goal->req.requestType == "planning")
+    {
+        client.sendMessage("move3d", "{\"ClearGTPInputs\":null}");
+        std::cout << client.getBlockingMessage().second << std::endl;
+        
+        
+        //_result = wrt.write(res);
+
+        
+        for (unsigned int i = 0; i< goal->req.involvedAgents.size(); i++)
+        {
+           Json::Value request(Json::objectValue);
+           Json::Value input(Json::objectValue);
+           
+           input["key"] = goal->req.involvedAgents.at(i).actionKey;
+           input["value"] = goal->req.involvedAgents.at(i).agentName;
+           request["AddGTPAgent"] = input;
+           
+           client.sendMessage("move3d", wrt.write(request));
+           ROS_INFO("planner answer: %s", client.getBlockingMessage().second.c_str());
+        }
+        
+        for (unsigned int i = 0; i< goal->req.involvedObjects.size(); i++)
+        {
+           Json::Value request(Json::objectValue);
+           Json::Value input(Json::objectValue);
+           
+           input["key"] = goal->req.involvedObjects.at(i).actionKey;
+           input["value"] = goal->req.involvedObjects.at(i).objectName;
+           request["AddGTPObject"] = input;
+           
+           client.sendMessage("move3d", wrt.write(request));
+           ROS_INFO("planner answer: %s", client.getBlockingMessage().second.c_str());
+        }
+        
+        for (unsigned int i = 0; i< goal->req.data.size(); i++)
+        {
+           Json::Value request(Json::objectValue);
+           Json::Value input(Json::objectValue);
+           
+           input["key"] = goal->req.data.at(i).dataKey;
+           input["value"] = goal->req.data.at(i).dataValue;
+           request["AddGTPData"] = input;
+           
+           client.sendMessage("move3d", wrt.write(request));
+           ROS_INFO("planner answer: %s", client.getBlockingMessage().second.c_str());
+        }
+        
+        for (unsigned int i = 0; i< goal->req.points.size(); i++)
+        {
+        
+           Json::Value request(Json::objectValue);
+           Json::Value input(Json::objectValue);
+           
+           input["x"] = goal->req.points.at(i).value.x;
+           input["y"] = goal->req.points.at(i).value.y;
+           input["z"] = goal->req.points.at(i).value.z;
+           
+           input["key"] = goal->req.points.at(i).pointKey;
+           request["AddGTPPoint"] = input;
+           
+           client.sendMessage("move3d", wrt.write(request));
+           ROS_INFO("planner answer: %s", client.getBlockingMessage().second.c_str());
+        }
+        
+        Json::Value request(Json::objectValue);
+        Json::Value input(Json::objectValue);
+        
+        input["computeMotionPlan"] = true;
+        input["previousTaskAlternativeId"] = (int)goal->req.predecessorId.alternativeId;
+        input["previousTaskId"] = (int)goal->req.predecessorId.actionId;
+        input["type"] = goal->req.actionName;
+        request["PlanGTPTask"] = input;
+        
+        client.sendMessage("move3d", wrt.write(request));
+        
+        std::string str = client.getBlockingMessage().second;
+        //ROS_INFO("%s",str.c_str());
+        
+        Json::Value root,nullval,tmpVal;
+        Json::Reader reader;
+        
+        bool parsedSuccess = reader.parse(str, root, false);
+        if(!parsedSuccess || root.size() <= 0)
+        {
+            ROS_INFO("Failed to parse JSON");
+            result_.ans.success = false;
+            as_.setSucceeded(result_);
+            return;
+        }
+        
+        
+        
+        tmpVal = root.get("PlanGTPTask", nullval );
+        if (tmpVal == nullval)
+        {
+           ROS_INFO("Failed to find PlanGTPTask");
+           result_.ans.success = false;
+           as_.setSucceeded(result_);
+           return;
+        }
+        
+
+        std::string report = tmpVal.get("returnReport", "Null" ).asString();
+        
+        
+        if (report == "OK")
+           result_.ans.success = true;
+        else
+        {
+           ROS_INFO("Failed to compute, reason: %s", report.c_str());
+           result_.ans.success = false;
+           as_.setSucceeded(result_);
+           return;
+        }
+          
+        result_.ans.identifier.actionId = tmpVal.get("taskId", "Null" ).asInt();
+        result_.ans.identifier.alternativeId = tmpVal.get("taskAlternativeId", "Null" ).asInt();
+        
+        
+        ROS_INFO("planner answer: %s", str.c_str());
+        as_.setSucceeded(result_);
+    }
+    else if (goal->req.requestType == "details")
+    {
+        Json::Value request(Json::objectValue);
+        Json::Value input(Json::objectValue);
+        
+        input["taskId"] = (int)goal->req.loadAction.actionId;
+        input["alternativeId"] = (int)goal->req.loadAction.alternativeId;
+        request["GetGTPDetails"] = input;
+        
+        client.sendMessage("move3d", wrt.write(request));
+        std::string res = client.getBlockingMessage().second;
+        //ROS_INFO("planner answer: %s", res.c_str());
+        
+        
+        Json::Value root, answer, nullval;
+        Json::Reader reader;
+  
+        reader.parse(res, root, false);
+        answer = root.get("GetGTPDetails", nullval );
+        
+        if (answer == nullval)
+        {
+          ROS_INFO("no GetGTPDetails");
+          result_.ans.success = false;
+          as_.setSucceeded(result_);
+          return;
+        }
+        
+        if (answer.get("status", "NULL").asString() != "OK")
+        {
+          ROS_INFO("get details failed");
+          result_.ans.success = false;
+          as_.setSucceeded(result_);
+          return;
+        }
+        
+        const Json::Value& subTrajs = answer["subTrajs"];
+        result_.ans.subTrajs.clear();
+        for (Json::ValueConstIterator it = subTrajs.begin(); it != subTrajs.end(); ++it)
+        {
+            const Json::Value& subTraj = *it;
+            // rest as before
+            gtp_ros_msg::SubTraj subMsg;
+            subMsg.agent = subTraj["agent"].asString();
+            subMsg.armId = subTraj["armId"].asInt();
+            subMsg.subTrajId = subTraj["subTrajId"].asInt();
+            subMsg.subTrajName = subTraj["subTrajName"].asString();
+            result_.ans.subTrajs.push_back(subMsg);
+        }
+        result_.ans.success = true;
+        as_.setSucceeded(result_);
+        
+    }
+    else if (goal->req.requestType == "load")
+    {
+        Json::Value request(Json::objectValue);
+        Json::Value input(Json::objectValue);
+        
+        input["taskId"] = (int)goal->req.loadAction.actionId;
+        input["alternativeId"] = (int)goal->req.loadAction.alternativeId;
+        input["subTrajId"] = (int)goal->req.loadAction.alternativeId;
+        request["GetGTPTraj"] = input;
+        
+        client.sendMessage("move3d", wrt.write(request));
+        std::string res = client.getBlockingMessage().second;
+        //ROS_INFO("planner answer: %s", res.c_str());
+        
+        
+        Json::Value root, nullval;
+        Json::Reader reader;
+        
+        reader.parse(res, root, false);
+        
+        gtp_ros_msg::GTPTraj t;
+        for (unsigned int i = 0; i < root["GetGTPTraj"]["confs"].size(); i++)
+        {
+            trajectory_msgs::JointTrajectoryPoint Jtpt;
+            for (unsigned int j = 0; j < root["GetGTPTraj"]["confs"][i].size(); j++)
+            {
+               Jtpt.positions.push_back(root["GetGTPTraj"]["confs"][i][j].asDouble());
+            }
+            t.traj.points.push_back(Jtpt);
+        }
+        
+        
+        t.name = "traj";
+        
+        //t.traj.points.
+        trajectory.publish(t);
+        
+        result_.ans.success = true;
+        as_.setSucceeded(result_);
+        
+    }
+    else if (goal->req.requestType == "update")
+    {
+       
+    }
+    else
+    {
+        ROS_INFO("request type: \"%s\" not supported, the supported types are: planning, details, load and update", goal->req.requestType.c_str());
+        result_.ans.success = false;
+        result_.ans.identifier.actionId = -1;
+        result_.ans.identifier.alternativeId = -1;
+        as_.setSucceeded(result_);
+    }
+        
+  }
+
+
+};
+
+
+
+//void chatterCallback(const std_msgs::String::ConstPtr& msg)
+//{
+//  ROS_INFO("I heard: [%s]", msg->data.c_str());
+//}
+
+
+
+int main(int argc, char** argv)
+{
+  std::string server = "localhost";
+  int port = 5500;
+  ROS_INFO("Starting node GTP_ROS with server: %s and port: %d",server.c_str(),port);
+  
+
+  ros::init(argc, argv, "gtp_ros_server_node");
+  
+  ros::NodeHandle n;
+  trajectory = n.advertise<gtp_ros_msg::GTPTraj>("gtp_trajectory", 1000);
+  
+  ros::Rate loop_rate(10);
+  
+
+
+//  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+
+  client.connect("gtp_ros_node", server, port);
+  if(client.isConnected())
+  {
+     client.sendMessage("move3d", "{\"InitGTP\":null}");
+     bool testMove3d = false;
+     int nbTestTime = 0;
+     int nbTestType = 0;
+     int maxtest = 3;
+     while (!testMove3d && nbTestTime < maxtest && nbTestType < maxtest)
+     {
+        std::pair<std::string,std::string> ans = client.getMessageTimeout(2000);
+        if (ans.second==STATUS_TIMEOUT)
+        {
+           nbTestTime++;
+           ROS_INFO("The server timed out %d times", nbTestTime);
+           continue;
+        }
+        if (ans.first != "move3d")
+        {
+           nbTestType++;
+           continue;
+        }
+        testMove3d = true;
+
+        GtpRosBridge bridge(ros::this_node::getName());
+        ros::spin();
+     }
+     if (!testMove3d)
+     {
+        if (nbTestTime >= maxtest)
+            ROS_INFO("no answer from move3d (time out reached): is it launched?");
+        if (nbTestType >= maxtest)
+            ROS_INFO("answer from other modules, might reconsider this code!!");
+     }
+
+  }
+  else
+  {
+     ROS_INFO("Failed to connect to geometric planner, abort");
+  }
+  
+  
+  
+
+
+  return 0;
+}
---
+++ src/gtp_ros_bridge_test_client.cpp
@@ -0,0 +1,294 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include <gtp_ros_msg/requestAction.h>
+#include <jsoncpp/json/json.h>
+
+#include <string>
+#include <fstream>
+#include <streambuf>
+
+using namespace std;
+
+int main (int argc, char **argv)
+{
+  ros::init(argc, argv, "test_client");
+  if (argc != 2) 
+  {
+    cout << "FAIL" << endl;
+    cout << "usage: rosrun gtp_ros_bridge gtp_ros_bridge_test_client [fileName]" << endl;
+    return 0;
+  }
+  string filename = argv[1];
+  
+  std::ifstream t(filename.c_str());
+  std::stringstream buffer;
+  buffer << t.rdbuf();
+  //cout << buffer.str() << endl;
+  
+  Json::Value root;
+  Json::Value nullVal;
+  Json::Reader reader;
+  
+  bool parsedSuccess = reader.parse(buffer.str(), root, false);
+  if(!parsedSuccess || root.size() <= 0)
+  {
+    // Report failures and their locations
+    // in the document.
+    cout<<"Failed to parse JSON"<< endl;
+    return 0;
+  }
+  
+  //Json::Value res(Json::objectValue);
+  
+
+  // create the action client
+  // true causes the client to spin its own thread
+  actionlib::SimpleActionClient<gtp_ros_msg::requestAction> ac("gtp_ros_server", true);
+
+  ROS_INFO("Waiting for action server to start.");
+  // wait for the action server to start
+  ac.waitForServer(); //will wait for infinite time
+
+  ROS_INFO("Action server started, sending goal.");
+  // send a goal to the action
+  gtp_ros_msg::requestGoal goal;
+  
+  std::string requesttype = root.get("requestType", "Null" ).asString();
+  if (requesttype == "Null")
+  {
+    cout << "no requestType" << endl;
+    return 0;
+  }
+  
+  goal.req.requestType = requesttype;
+  
+  if (requesttype == "planning")
+  {
+  
+      std::string type = root.get("actionType", "Null" ).asString();
+      if (type == "Null")
+      {
+        cout << "no actionType" << endl;
+        return 0;
+      }
+      //cout << "type = " << type << endl;
+      goal.req.actionName = type;
+      
+      int Id = root.get("previousId", -170 ).asInt();
+      if (Id == -170)
+      {
+        cout << "no previousId" << endl;
+        return 0;
+      }
+      goal.req.predecessorId.actionId = Id;
+      
+      int altId = root.get("previousAltId", -170 ).asInt();
+      if (altId == -170)
+      {
+        cout << "no previousAltId" << endl;
+        return 0;
+      }
+      goal.req.predecessorId.alternativeId = altId;
+      
+      Json::Value inputVal = root.get("input", nullVal );
+      if (inputVal == nullVal)
+      {
+        cout << "no inputs" << endl;
+        return 0;
+      }
+      
+      Json::Value agentVal = inputVal.get("agents", nullVal );
+      if (agentVal != nullVal)
+      {
+        for (unsigned int i = 0; i < agentVal.getMemberNames().size(); i++)
+        {
+          gtp_ros_msg::Ag agent;
+          Json::Value tmp = agentVal.get(agentVal.getMemberNames().at(i), nullVal );
+          agent.actionKey = tmp.get("key", "null").asString();
+          agent.agentName = tmp.get("value", "null").asString();
+          goal.req.involvedAgents.push_back(agent);
+        }
+      }
+      
+      Json::Value objectVal = inputVal.get("objects", nullVal );
+      if (objectVal != nullVal)
+      {
+        for (unsigned int i = 0; i < objectVal.getMemberNames().size(); i++)
+        {
+          gtp_ros_msg::Obj object;
+          Json::Value tmp = objectVal.get(objectVal.getMemberNames().at(i), nullVal );
+          object.actionKey = tmp.get("key", "null").asString();
+          object.objectName = tmp.get("value", "null").asString();
+          goal.req.involvedObjects.push_back(object);
+        }
+      }
+      
+      Json::Value dataVal = inputVal.get("datas", nullVal );
+      if (dataVal != nullVal)
+      {
+        for (unsigned int i = 0; i < dataVal.getMemberNames().size(); i++)
+        {
+          gtp_ros_msg::Data data;
+          Json::Value tmp = dataVal.get(dataVal.getMemberNames().at(i), nullVal );
+          data.dataKey = tmp.get("key", "null").asString();
+          data.dataValue = tmp.get("value", "null").asString();
+          goal.req.data.push_back(data);
+        }
+      }
+      
+      Json::Value pointVal = inputVal.get("points", nullVal );
+      if (pointVal != nullVal)
+      {
+        for (unsigned int i = 0; i < pointVal.getMemberNames().size(); i++)
+        {
+          gtp_ros_msg::Points ptt;
+          Json::Value tmp = pointVal.get(pointVal.getMemberNames().at(i), nullVal );
+          ptt.pointKey = tmp.get("key", "null").asString();
+          if (tmp.get("value", "null").size() != 3)
+          {
+            cout << "problem in point with id: " << pointVal.getMemberNames().at(i) << endl;
+            return 0;
+          }
+          ptt.value.x = tmp.get("value", "null")[0].asDouble();
+          ptt.value.y = tmp.get("value", "null")[1].asDouble();
+          ptt.value.z = tmp.get("value", "null")[2].asDouble();
+          goal.req.points.push_back(ptt);
+        }
+      }
+      
+      
+      
+      //cout << root.toStyledString() << endl;
+      
+      //for (unsigned int i = 0; i < root.getMemberNames().size(); i++)
+      //{
+      //  cout << root.getMemberNames().at(i) << endl;
+      //}
+
+
+      ac.sendGoal(goal);
+
+    //  //wait for the action to return
+      bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
+
+      if (finished_before_timeout)
+      {
+        actionlib::SimpleClientGoalState state = ac.getState();
+        ROS_INFO("Action finished: %s", state.toString().c_str());
+        if (ac.getResult()->ans.success)
+        {  
+            ROS_INFO("Action computed with success");
+            ROS_INFO("id = %lu, altId = %lu", ac.getResult()->ans.identifier.actionId, ac.getResult()->ans.identifier.alternativeId);
+        }
+        else
+        {
+            ROS_INFO("No path found");
+        }
+            
+      }
+      else
+        ROS_INFO("Action did not finish before the time out.");
+
+      //exit
+      return 0;
+  }
+  else if (requesttype == "details")
+  {
+      int actionId = root.get("actionId", -1 ).asInt();
+      if (actionId == -1)
+      {
+        cout << "no actionId" << endl;
+        return 0;
+      }
+      
+      int altId = root.get("altId", -1 ).asInt();
+      if (altId == -1)
+      {
+        cout << "no actionId" << endl;
+        return 0;
+      }
+      
+      goal.req.loadAction.actionId = actionId;
+      goal.req.loadAction.alternativeId = altId;
+      
+      ac.sendGoal(goal);
+
+    //  //wait for the action to return
+      bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
+
+      if (finished_before_timeout)
+      {
+        actionlib::SimpleClientGoalState state = ac.getState();
+        ROS_INFO("Action finished: %s", state.toString().c_str());
+        if (ac.getResult()->ans.success)
+        {  
+            ROS_INFO("Details retrieved with success");
+            ROS_INFO("nb subtrajs = %lu", ac.getResult()->ans.subTrajs.size());
+        }
+        else
+        {
+            ROS_INFO("No path found");
+        }
+            
+      }
+      else
+        ROS_INFO("Action did not finish before the time out.");
+
+      //exit
+      return 0;
+      
+  }
+  else if (requesttype == "load")
+  {
+      int actionId = root.get("actionId", -1 ).asInt();
+      if (actionId == -1)
+      {
+        cout << "no actionId" << endl;
+        return 0;
+      }
+      
+      int altId = root.get("altId", -1 ).asInt();
+      if (altId == -1)
+      {
+        cout << "no actionId" << endl;
+        return 0;
+      }
+      
+      int subTrajId = root.get("subTrajId", -1 ).asInt();
+      if (subTrajId == -1)
+      {
+        cout << "no subTrajId" << endl;
+        return 0;
+      }
+      
+      goal.req.loadAction.actionId = actionId;
+      goal.req.loadAction.alternativeId = altId;
+      goal.req.loadSubTraj = subTrajId;
+      
+      ac.sendGoal(goal);
+  
+  
+  }
+  else if (requesttype == "update")
+  {
+  }
+  else
+  {
+    ROS_INFO("request type: %s unknown, abort action.", requesttype.c_str());
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
